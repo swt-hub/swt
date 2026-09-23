461aaa8b896ae9ab8b8776cccafa33784cbf920c
swt-hub <swt1secretary@gmail.com>
2026-09-23 22:49:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+
+/* ===== تأثير الهيدر عند التمرير ===== */
+
+window.addEventListener("scroll", function () {
+
+    const nav = document.querySelector(".nav");
+
+    if (window.scrollY > 30) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+
+}, { passive: true });
